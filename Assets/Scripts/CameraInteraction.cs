a51f696d910b425c4f08301d6eab82b81cf0e9bb
a-adel-dev <58854098+a-adel-dev@users.noreply.github.com>
2020-12-17 11:27:53 +0200

    added Camera Controls

---
+++ Assets/Scripts/CameraInteraction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraInteraction : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        CameraController.instance.followTransform = transform;
+        Debug.Log(this.gameObject.name);
+    }
+}
