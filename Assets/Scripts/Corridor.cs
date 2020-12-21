bce63dcb59653190b6cd1e7c6736af551a69f82e
a-adel-dev <58854098+a-adel-dev@users.noreply.github.com>
2020-12-21 11:57:35 +0200

    added a rocky stop for POI

--- Assets/Scripts/Corridor.cs
+++ Assets/Scripts/Corridor.cs
@@ -1,21 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Corridor : MonoBehaviour
 {
     bool classesInsession = false;
     SchoolManager schoolManager;
+    List<Spot> pointsOfInterest = new List<Spot>();
+    List<AI> pupilsInCorridors = new List<AI>();
     
     // Start is called before the first frame update
     void Start()
     {
         schoolManager = FindObjectOfType<SchoolManager>();
+        foreach (Transform child in transform)
+        {
+            pointsOfInterest.Add(child.GetComponent<Spot>());
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         classesInsession = schoolManager.classInSession;
+
+        
+    }
+
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pupil"))
+        {
+            pupilsInCorridors.Add(other.GetComponent<AI>()) ;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Pupil"))
+        {
+            pupilsInCorridors.Remove(other.GetComponent<AI>());
+        }
     }
 }
---
+++ Assets/Scripts/POI.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class POI : MonoBehaviour
+{
+    List<AI> pupilsNearPOI = new List<AI>();
+    SchoolManager schoolManager;
+    float timeStep;
+    // Start is called before the first frame update
+    void Start()
+    {
+        schoolManager = FindObjectOfType<SchoolManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStep = schoolManager.GetTimeStep();
+        StartCoroutine(StopPupils());
+    }
+
+    IEnumerator StopPupils()
+    {
+        foreach (AI pupil in pupilsNearPOI)
+        {
+            if (Vector3.Distance(pupil.transform.position, transform.position) < 3f)
+            {
+                pupil.GetComponent<NavMeshAgent>().isStopped = true;
+                yield return new WaitForSeconds(2f * timeStep);
+                pupil.GetComponent<NavMeshAgent>().isStopped = false;
+                yield return new WaitForSeconds(3f);
+            }
+        }
+        yield return new WaitForSeconds(2f * timeStep);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pupil"))
+        {
+            pupilsNearPOI.Add(other.GetComponent<AI>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Pupil"))
+        {
+            pupilsNearPOI.Remove(other.GetComponent<AI>());
+        }
+    }
+}
